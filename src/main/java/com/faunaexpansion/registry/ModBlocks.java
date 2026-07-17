9b4a5eba3825235900bca821af9dbf4180770425
Owsper <rohanftw2466@gmail.com>
2026-07-18 00:33:47 +0200

    Initial commit

---
+++ src/main/java/com/faunaexpansion/registry/ModBlocks.java
@@ -0,0 +1,4 @@
+package com.faunaexpansion.registry;
+
+public class ModBlocks {
+}
---
+++ src/main/java/com/faunaexpansion/registry/ModItems.java
@@ -0,0 +1,11 @@
+package com.faunaexpansion.registry;
+
+import com.faunaexpansion.FaunaExpansion;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+}

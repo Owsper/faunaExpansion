aae49ff1fb020a7075a9bd7c3eb10e13bdda2903
Owsper <rohanftw2466@gmail.com>
2026-07-18 11:09:13 +0200

    Wet Sand Block registry

--- src/main/java/com/faunaexpansion/FaunaExpansion.java
+++ src/main/java/com/faunaexpansion/FaunaExpansion.java
@@ -2,29 +2,12 @@ package com.faunaexpansion;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.util.Identifier;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class FaunaExpansion implements ModInitializer {
-	public static final String MOD_ID = "fauna-expansion";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "fauna-expansion";
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
-
-	public static Identifier id(String path) {
-		return new Identifier(MOD_ID, path);
-	}
+    @Override
+    public void onInitialize() {
+        System.out.println("Fauna Expansion loaded!");
+    }
 }
--- src/main/java/com/faunaexpansion/registry/ModBlocks.java
+++ src/main/java/com/faunaexpansion/registry/ModBlocks.java
@@ -1,4 +1,20 @@
 package com.faunaexpansion.registry;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+
 public class ModBlocks {
+
+
+    public static final Block WET_SAND = new Block(
+            FabricBlockSettings.copyOf(Blocks.SAND)
+    );
+
+
+    public static void registerBlocks() {
+        Registry.register(Registry.BLOCK, new Identifier("faunaexpansion", "wet_sand"), WET_SAND);
+    }
 }
